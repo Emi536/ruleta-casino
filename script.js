57c88bb90a7e075eb6eb7de4326d4ba001ce2acb
Emi536 <paze0410@gmail.com>
2025-05-21 12:08:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 let fill = document.getElementById("progress-fill");
-let bubble = document.getElementById("progress-bubble");
+let text = document.getElementById("progress-text");
 let percent = 0;
 
 let interval = setInterval(() => {
     percent++;
-
-    // Actualiza el ancho de la barra
     fill.style.width = percent + "%";
-
-    // Actualiza el texto de la burbuja
-    bubble.innerText = percent + "%";
-
-    // Mueve la burbuja al final de la barra
-    bubble.style.left = percent + "%";
-
+    text.innerText = "Cargando... " + percent + "%";
     if (percent >= 100) {
         clearInterval(interval);
     }
 }, 40);
+
+este es mi script.js
